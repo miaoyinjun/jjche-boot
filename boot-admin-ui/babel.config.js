90361390c14f9e23dcfb1d0c38f4069d0e116a16
miaoyinjun <312656362@qq.com>
2021-12-24 10:35:26 +0800

    前端-参照vue-element-admin Update to vue-cli@4

--- boot-admin-ui/babel.config.js
+++ boot-admin-ui/babel.config.js
@@ -4,8 +4,16 @@ if (process.env.NODE_ENV === 'production') {
   plugins.push('transform-remove-console')
 }
 module.exports = {
-  plugins: plugins,
   presets: [
-    '@vue/app'
-  ]
+    // https://github.com/vuejs/vue-cli/tree/master/packages/@vue/babel-preset-app
+    '@vue/cli-plugin-babel/preset'
+  ],
+  'env': {
+    'development': {
+      // babel-plugin-dynamic-import-node plugin only does one thing by converting all import() to require().
+      // This plugin can significantly increase the speed of hot updates, when you have a large number of pages.
+      // https://panjiachen.github.io/vue-element-admin-site/guide/advanced/lazy-loading.html
+      'plugins': ['dynamic-import-node']
+    }
+  }
 }
