cdb20da2b30b2ce5532aa72755e82c840396b7b4
miaoyinjun <312656362@qq.com>
2022-01-27 14:14:11 +0800

    解决JacksonHttpMessageConverter不起作用，也可将包名修改为非org开头解决

--- jjche-boot/jjche-boot-parent/jjche-boot-eladmin/jjche-boot-eladmin-system/src/main/java/org/jjche/AdminAutoConfiguration.java
+++ jjche-boot/jjche-boot-parent/jjche-boot-eladmin/jjche-boot-eladmin-system/src/main/java/org/jjche/AdminAutoConfiguration.java
@@ -1,11 +1,8 @@
 package org.jjche;
 
 import org.jjche.common.constant.PackageConstant;
-import org.jjche.jackson.JacksonAutoConfiguration;
 import org.jjche.log.biz.starter.annotation.EnableLogRecord;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 /**
  * <p>
@@ -15,7 +12,7 @@ import org.springframework.context.annotation.FilterType;
  * @author miaoyj
  * @since 2022-01-05
  */
-@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {JacksonAutoConfiguration.class})})
+//@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {SecurityBasicAuthFilter.class})})
 @MapperScan(PackageConstant.MAPPER_PATH_STAR)
 @EnableLogRecord(tenant = PackageConstant.BASE_PATH)
 public class AdminAutoConfiguration {
