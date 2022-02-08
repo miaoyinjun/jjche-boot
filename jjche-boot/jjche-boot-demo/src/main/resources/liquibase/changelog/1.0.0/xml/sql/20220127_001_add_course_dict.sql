6bd4202ad660c81824588e88830ef651a7e89ed4
miaoyinjun <312656362@qq.com>
2022-02-08 13:47:49 +0800

    jjche-boot-demo，liquibase兼容spring-cloud

---
+++ jjche-boot/jjche-boot-demo/src/main/resources/liquibase/changelog/1.0.0/xml/sql/20220127_001_add_course_dict.sql
@@ -0,0 +1,14 @@
+INSERT INTO `sys_dict`
+(`name`, `description`, `created_by`, `updated_by`, `gmt_create`, `gmt_modified`)
+VALUES ('course_status', '课程状态', 'System', 'System', '2021-09-13 13:15:30', '2021-09-13 13:15:30');
+
+INSERT INTO `sys_dict_detail`
+(`dict_id`, `label`, `value`, `dict_sort`)
+VALUES ((SELECT id FROM sys_dict WHERE name = 'course_status'),
+'图文', '102', 1),
+((SELECT id FROM sys_dict WHERE name = 'course_status'),
+'音频', '103', 2),
+((SELECT id FROM sys_dict WHERE name = 'course_status'),
+'视频', '104', 3),
+((SELECT id FROM sys_dict WHERE name = 'course_status'),
+'外链', '105', 4);
