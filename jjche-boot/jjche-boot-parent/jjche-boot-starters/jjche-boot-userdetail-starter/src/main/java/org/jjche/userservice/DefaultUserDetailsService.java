b7ecb7e7a86dc0748b5b3d1740ad0098afcce986
miaoyinjun <312656362@qq.com>
2022-02-11 09:21:42 +0800

    分离defaultUserDetailService

--- jjche-boot/jjche-boot-parent/jjche-boot-starters/jjche-boot-userdetail-starter/src/main/java/org/jjche/userservice/DefaultUserDetailsService.java
+++ jjche-boot/jjche-boot-parent/jjche-boot-starters/jjche-boot-userdetail-starter/src/main/java/org/jjche/userservice/DefaultUserDetailsService.java
@@ -1,13 +1,11 @@
-package org.jjche.cloud.userservice;
+package org.jjche.userservice;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service("userDetailsService")
-@ConditionalOnMissingBean(type = "userDetailsService")
 public class DefaultUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
