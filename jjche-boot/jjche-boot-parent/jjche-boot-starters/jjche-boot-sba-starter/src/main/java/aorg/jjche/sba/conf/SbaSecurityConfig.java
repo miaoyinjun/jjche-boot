8a47759fd43d859a5cfd8ec59502c3836c3765b7
miaoyinjun <312656362@qq.com>
2022-03-09 15:52:11 +0800

    cloud版本下，sba/**在任何环境下不做安全验证

--- jjche-boot/jjche-boot-parent/jjche-boot-starters/jjche-boot-sba-starter/src/main/java/aorg/jjche/sba/conf/SbaSecurityConfig.java
+++ jjche-boot/jjche-boot-parent/jjche-boot-starters/jjche-boot-sba-starter/src/main/java/aorg/jjche/sba/conf/SbaSecurityConfig.java
@@ -1,13 +1,13 @@
 package aorg.jjche.sba.conf;
 
+import cn.hutool.core.util.BooleanUtil;
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import org.jjche.common.constant.EnvConstant;
+import org.jjche.core.util.SpringContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,36 +30,10 @@ public class SbaSecurityConfig {
 
     /**
      * <p>
-     * dev 环境加载
-     * </p>
-     */
-    @Profile(EnvConstant.DEV)
-    @Configuration
-    @Order(1)
-    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-        private final String adminContextPath;
-
-        public SecurityPermitAllConfig(AdminServerProperties adminServerProperties) {
-            this.adminContextPath = adminServerProperties.getContextPath();
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable()
-                    .headers().frameOptions().disable();
-            /**解决与其它安全配置冲突问题*/
-            http.antMatcher(adminContextPath + "/**").authorizeRequests()
-                    .anyRequest()
-                    .permitAll();
-        }
-    }
-
-    /**
-     * <p>
      * qa,demo,prod 环境加载
+     * cloud版本全部放开
      * </p>
      */
-    @Profile({EnvConstant.QA, EnvConstant.DEMO, EnvConstant.PROD})
     @Configuration
     @Order(1)
     public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
@@ -79,53 +53,49 @@ public class SbaSecurityConfig {
 
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            PasswordEncoder encoder = new BCryptPasswordEncoder();
-
-            SecurityProperties.User user = securityProperties.getUser();
-            String username = user.getName();
-            String password = user.getPassword();
-            password = encoder.encode(password);
-            auth.inMemoryAuthentication()
-                    .withUser(username)
-                    .password(password)
-                    .roles("ADMIN");
+            boolean isNotDev = !SpringContextHolder.isDev();
+            //单体版才支持登录
+            if (BooleanUtil.isFalse(SpringContextHolder.isCloud()) && isNotDev) {
+                PasswordEncoder encoder = new BCryptPasswordEncoder();
+                SecurityProperties.User user = securityProperties.getUser();
+                String username = user.getName();
+                String password = user.getPassword();
+                password = encoder.encode(password);
+                auth.inMemoryAuthentication().withUser(username).password(password).roles("ADMIN");
+            }
         }
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-            //登录跳转
-            successHandler.setTargetUrlParameter("redirectTo");
-//            successHandler.setDefaultTargetUrl(adminContextPath);
-//            successHandler.setAlwaysUseDefaultTargetUrl(true);
+            //单体版才支持登录
+            if (SpringContextHolder.isCloud() || SpringContextHolder.isDev()) {
+                http.csrf().disable().headers().frameOptions().disable();
+                /**解决与其它安全配置冲突问题*/
+                http.antMatcher(adminContextPath + "/**").authorizeRequests().anyRequest().permitAll();
+            } else {
+                SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+                //登录跳转
+                successHandler.setTargetUrlParameter("redirectTo");
 
-            http.authorizeRequests()
-                    .antMatchers(adminContextPath + "/assets/**").permitAll()
-                    .antMatchers(adminContextPath + "/login").permitAll()
+                http.authorizeRequests().antMatchers(adminContextPath + "/assets/**").permitAll().antMatchers(adminContextPath + "/login").permitAll()
 //					.anyRequest().authenticated()
 //                    .antMatchers(adminContextPath + "/**").authenticated()
-                    .and()
-                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
-                    .logout().logoutUrl(adminContextPath + "/logout").and()
-                    .httpBasic().and()
-                    .rememberMe().and()
-                    .csrf().disable()
-                    .headers().frameOptions().disable();
-            /**解决与其它安全配置冲突问题*/
-            http.antMatcher(adminContextPath + "/**").authorizeRequests()
-                    .anyRequest().authenticated();
+                        .and().formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and().logout().logoutUrl(adminContextPath + "/logout").and().httpBasic().and().rememberMe().and().csrf().disable().headers().frameOptions().disable();
+                /**解决与其它安全配置冲突问题*/
+                http.antMatcher(adminContextPath + "/**").authorizeRequests().anyRequest().authenticated();
+            }
         }
     }
 
-    /**
-     * 解决 httpTrace失效
-     *
-     * @return a {@link org.springframework.boot.actuate.trace.http.HttpTraceRepository} object.
-
-    //    @ConditionalOnMissingBean
-    //    @Bean
-    //    public HttpTraceRepository httpTraceRepository() {
-    //        return new InMemoryHttpTraceRepository();
-    //    }
-     */
+/**
+ * 解决 httpTrace失效
+ *
+ * @return a {@link org.springframework.boot.actuate.trace.http.HttpTraceRepository} object.
+ * <p>
+ * //    @ConditionalOnMissingBean
+ * //    @Bean
+ * //    public HttpTraceRepository httpTraceRepository() {
+ * //        return new InMemoryHttpTraceRepository();
+ * //    }
+ */
 }
