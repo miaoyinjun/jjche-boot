6a96bdee8f9c2b56634cbaabde966acd1350597a
miaoyinjun <312656362@qq.com>
2022-03-15 13:04:03 +0800

    增加模块-jjch-cloud-monitor

---
+++ jjche-boot/jjche-cloud/jjche-cloud-monitor/src/test/java/SpringBootActuatorApplicationTests.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootActuatorApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
