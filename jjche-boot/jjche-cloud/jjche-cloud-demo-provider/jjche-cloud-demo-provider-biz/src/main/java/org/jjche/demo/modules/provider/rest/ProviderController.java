d63754ffac15b62df7b15ff2e4b50eb5d7b05557
miaoyinjun <312656362@qq.com>
2022-04-29 11:23:45 +0800

    ProviderController增加被调用测试日志

--- jjche-boot/jjche-cloud/jjche-cloud-demo-provider/jjche-cloud-demo-provider-biz/src/main/java/org/jjche/demo/modules/provider/rest/ProviderController.java
+++ jjche-boot/jjche-cloud/jjche-cloud-demo-provider/jjche-cloud-demo-provider-biz/src/main/java/org/jjche/demo/modules/provider/rest/ProviderController.java
@@ -5,6 +5,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.jjche.common.enums.LogCategoryType;
+import org.jjche.common.enums.LogType;
 import org.jjche.common.param.MyPage;
 import org.jjche.common.param.PageParam;
 import org.jjche.common.response.response.ResultWrapper;
@@ -15,6 +17,7 @@ import org.jjche.demo.modules.provider.api.enums.ProviderCourseEnum;
 import org.jjche.demo.modules.provider.api.vo.ProviderVO;
 import org.jjche.demo.modules.provider.feign.ProviderApi;
 import org.jjche.demo.modules.provider.service.ProviderService;
+import org.jjche.log.biz.starter.annotation.LogRecordAnnotation;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,6 +42,10 @@ public class ProviderController extends BaseController implements ProviderApi {
     @GetMapping
     @ApiOperation(value = "学生-列表", tags = ProviderApiVersion.VERSION_1_0_0)
     @PreAuthorize("@el.check('student:list')")
+    @LogRecordAnnotation(
+            value = "被调用", category = LogCategoryType.OPERATING,
+            type = LogType.SELECT, module = "学生"
+    )
     public ResultWrapper<MyPage<ProviderVO>> page(PageParam page,
                                                   @ApiParam(value = "课程")
                                                   @RequestParam(required = false) ProviderCourseEnum course,
