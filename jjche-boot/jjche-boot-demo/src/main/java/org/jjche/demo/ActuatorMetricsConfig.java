abc37232f6e0bebc3df88f9abcc85ea4a5aaa59f
miaoyinjun <312656362@qq.com>
2022-07-08 16:52:53 +0800

    支持Prometheus监控，地址：http://localhost:8081/sba/monitor/prometheus

---
+++ jjche-boot/jjche-boot-demo/src/main/java/org/jjche/demo/ActuatorMetricsConfig.java
@@ -0,0 +1,17 @@
+package org.jjche.demo;
+
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ActuatorMetricsConfig {
+
+    @Bean
+    InitializingBean forcePrometheusPostProcessor(BeanPostProcessor meterRegistryPostProcessor, PrometheusMeterRegistry registry) {
+        return () -> meterRegistryPostProcessor.postProcessAfterInitialization(registry, "");
+    }
+
+}
