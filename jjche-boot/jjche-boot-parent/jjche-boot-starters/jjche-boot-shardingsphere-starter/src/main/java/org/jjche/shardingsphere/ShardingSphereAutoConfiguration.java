d7931eb39e8770eb60538143e8012c0956fe4f7a
miaoyinjun <312656362@qq.com>
2022-08-26 14:02:40 +0800

    增加-shardingsphere-读写分离

---
+++ jjche-boot/jjche-boot-parent/jjche-boot-starters/jjche-boot-shardingsphere-starter/src/main/java/org/jjche/shardingsphere/ShardingSphereAutoConfiguration.java
@@ -0,0 +1,19 @@
+package org.jjche.shardingsphere;
+
+import org.jjche.shardingsphere.conf.DruidConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * <p>
+ * 入口
+ * </p>
+ *
+ * @author miaoyj
+ * @version 1.0.0-SNAPSHOT
+ * @since 2020-07-09
+ */
+@Import({DruidConfig.class})
+@Configuration
+public class ShardingSphereAutoConfiguration {
+}
